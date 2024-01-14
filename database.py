683c5c194b81bdcb162cbc31729666333701cf75
BISWAJIT GHOSAL <ghosalbiswajit11@gmail.com>
2024-01-15 02:21:35 +0530

    loaded jobs from database

--- database.py
+++ database.py
@@ -1,15 +1,25 @@
-from sqlalchemy import create_engine, text
+from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
-connection_string = os.environ.get('DATABASE_URI')
-engine = create_engine(connection_string)
+# Assuming your MongoDB connection string is stored in the environment variable 'MONGODB_URI'
+mongo_uri = os.environ.get('MONGODB_URI')
+client = MongoClient(mongo_uri)
+
+# Select your database and collection
+db = client.get_database("career")
+collection = db["jobs"]
 
 
 def load_jobs():
-    with engine.connect() as con:
-        result = con.execute(text("SELECT * FROM jobs"))
-        column_names = result.keys()  # Get column names
-        jobs = [dict(zip(column_names, row)) for row in result.fetchall()]
-        return jobs
+    # Find all documents in the 'jobs' collection
+    jobs = list(collection.find())
+    return jobs
+
+
+# Example usage:
+jobs_data = load_jobs()
+for job in jobs_data:
+    print(job)
