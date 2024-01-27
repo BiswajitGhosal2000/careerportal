ad37e664b82412dfda2ede5d2301fc8f75fcb8e7
BISWAJIT GHOSAL <ghosalbiswajit11@gmail.com>
2024-01-27 22:04:52 +0530

    fully database driven application

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
 import database
-from database import load_jobs, get_job_by_id
+from database import load_jobs, get_job_by_id, apply_job
 
 app = Flask(__name__)
 
@@ -26,6 +26,13 @@ def job_by_id(jobid):
     if not job:
         return "Not Found", 404
     return render_template('jobpage.html', job=job)
+
+
+@app.route('/job/<jobid>/apply')
+def apply(jobid):
+    alert = apply_job(jobid, request.args)
+    jobs = load_jobs()
+    return render_template('index.html', jobs=jobs, alert=alert)
 
 
 app.run(host="0.0.0.0", debug=True)
--- database.py
+++ database.py
@@ -1,7 +1,10 @@
+from flask import jsonify
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from bson import ObjectId
 import os
+from werkzeug.datastructures import MultiDict
+import json
 
 load_dotenv()
 
@@ -33,4 +36,27 @@ def get_job_by_id(job_id):
     return job
 
 
-# print(get_job_by_id("65a4483753d7e44af4b59ac5"))
+def apply_job(jobid, form_data):
+    collection_jobs = db['jobs']
+    job = collection_jobs.find_one({'_id': ObjectId(jobid)})
+
+    if not job:
+        return "Not Found"
+    else:
+        # Convert the ImmutableMultiDict to a mutable MultiDict
+        data = MultiDict(form_data)
+        # Add job_id to the application data
+        data.add("job_id", ObjectId(jobid))
+        data.add("_id", ObjectId())
+        data_dict = dict(data)
+        # Convert the dictionary to JSON
+        # json_data = json.dumps(data_dict, indent=2)
+
+        collection_applications = db['applications']
+        response = collection_applications.insert_one(data_dict)
+        return response.inserted_id
+
+
+# print(apply_job("65a4483753d7e44af4b59ac5", []))
+
+
